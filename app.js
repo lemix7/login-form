462d44dc2913342ac7f56b16c40f293b9b17502d
Ahmad <ahmedgeger@gmail.com>
2024-08-27 07:35:40 +0300

    form validation3

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ let form = document.querySelector('form')
 let error = document.querySelector('.error-msg')
 let errorMsgs = []
 
+let nameError = document.getElementById('name-error')
+let emailError = document.getElementById('email-error')
+let passError = document.getElementById('pass-1-error')
+let pass2Error = document.getElementById('pass-2-error')
+
 
 
 form.addEventListener('submit', (e) => {
@@ -21,30 +26,33 @@ form.addEventListener('submit', (e) => {
 
     if(name.value === ''){
         errorMsgs.push('name is required')
+        nameError.textContent = 'Name is required'
     }
     if(email.value === ''){
         errorMsgs.push('email is required')
+        emailError.textContent = 'Email is required'
     }
     if(password.value === '' || password.value.length <= 8){
         errorMsgs.push('password must be more than 8 characters')
+        passError.textContent = 'password must be more than 8 characters'
     }
-     if(passwordReEnter.value = '' ){
+     if(passwordReEnter.value == '' || password.value !== passwordReEnter.value ){
         errorMsgs.push(`passwords don't match`)
+        pass2Error.textContent = `passwords don't match`
     }
-
 
     if(errorMsgs.length > 0){
         e.preventDefault()
-        error.innerText = errorMsgs.join(', ')
     }
-
-    // console.log(error.value.length);
     console.log(errorMsgs);
-    console.log(errorMsgs.length);
 
 })
 
 function updateForm(){
+    nameError.textContent = ''
+    emailError.textContent = ''
+    passError.textContent = ''
+    pass2Error.textContent = ''
     errorMsgs = []
 }
 
